ed0ee16d6f1f10e6c012503c4bccf3e26c6f50ee
Cristel Capetillo <cristel.capetillo@outlook.com>
2021-01-18 20:51:18 +0100

    Creates RunePurchase.cs
    Script is not attached to PurchaseButton just yet. Small changes to OwnedRunes Panel.

--- Assets/Scripts/RuneScriptObjects/RunePurchase.cs
+++ Assets/Scripts/RuneScriptObjects/RunePurchase.cs
@@ -1,18 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Runes;
 using UnityEngine;
 
 public class RunePurchase : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    // Some ideas, still unsure on how to add the Purchase functionality to PurchaseButton...
+    // When OnClick, 4 random scriptable objects -runes- should appear, don't know how to do it, yet.
+    public RuneScriptObjects[] runeList = new RuneScriptObjects[15];
+   
+       public void PurchaseRune(RuneScriptObjects rune)
+       {
+           for (int i = 0; i < runeList.Length; i++)
+           {
+               if (runeList[i] == null)
+               {
+                   runeList[i] = rune;
+                   return;
+               }
+           }
+       }
 }
