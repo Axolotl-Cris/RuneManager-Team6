834d47c639968faf70ce99389e799f2ee1c838b3
Cristel Capetillo <cristel.capetillo@outlook.com>
2021-01-18 16:53:05 +0100

    Adds more RuneSlots & component GridLayout

--- Assets/Scripts/UI/RunePurchase.cs
+++ Assets/Scripts/UI/RunePurchase.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PurchaseHandler : MonoBehaviour
+public class RunePurchase : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
