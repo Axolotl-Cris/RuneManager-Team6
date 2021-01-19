848c3aba5b727a32aae651577241e62fa14c860d
Linus Vallinder <divinator.dev@gmail.com>
2021-01-19 18:52:36 +0100

    Added Merge Button and some code

--- Assets/Scripts/Runes/RuneMerger.cs
+++ Assets/Scripts/Runes/RuneMerger.cs
@@ -1,25 +1,87 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
-/*
 namespace Runes
 {
-*/
     public class RuneMerger : MonoBehaviour
     {
-        //Runes selected ´ro be merged
+        //Runes selected to be merged
         public List<RuneScriptObjects> SelectedObjects = new List<RuneScriptObjects>();
 
         public void Merge()
         {
-            int RandomNumber = Random.Range(1, 100);
+            if (SelectedObjects.Count < 2 && IsOfSameRarity(SelectedObjects))
+            {
+                return;
+            }
+
+            switch (SelectedObjects.Count)
+            {
+                case 2:
+
+                    UpgradeRune(20);
+
+                    break;
+
+                case 3:
+
+                    UpgradeRune(55);
+
+                    break;
+
+                case 4:
+
+                    UpgradeRune(95);
+
+                    break;
+            }
 
             //TODO: Check selected objetc amount
             //Check if runes are of same rarity
             //Check if number is within Merge Range
             //Return random rune
+        }
 
+        private void UpgradeRune(int chance)
+        {
+            if (UpgradeChance(chance))
+            {
+                Debug.Log($"You upgraded a rune with a {chance} percent chance!");
+            }
+            else
+            {
+                if (!UpgradeChance(chance))
+                {
+                    Debug.Log($"You failed to upgrade a rune with a {chance} percent chance!");
+                }
+            }
+        }
+
+        internal bool IsOfSameRarity(List<RuneScriptObjects> selectedRunes)
+        {
+            if (selectedRunes.Distinct().Count() == 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        internal bool UpgradeChance(int percentChance)
+        {
+            int RandomNumber = Random.Range(1, 100);
+
+            if (RandomNumber <= percentChance)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
-//}
+}
