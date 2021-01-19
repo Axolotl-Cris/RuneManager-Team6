b03ab823611f9ca5233af9538972e0e979902e1e
Cristel Capetillo <cristel.capetillo@outlook.com>
2021-01-19 11:51:08 +0100

    Rearranges the folders
    Just placed the RunesScriptObj folder in Assets, the RuneMerger and RunePurchase in Runes folder, under Scripts. The MainMenu script now is on UI folder, under Scripts.

--- Assets/Scripts/CardBasic.cs
+++ Assets/Scripts/CardBasic.cs
@@ -7,10 +7,10 @@ public class CardBasic : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        CardBasic myNeatCard;
-        string Health;
-        string Damage;
-        string Tier;
+        //CardBasic myNeatCard;
+        //string Health;
+        //string Damage;
+        //string Tier;
         //DescriptionTag = "CARD_DESCRIBTION";
         //Effects = new List<CardEffectModel;
     }
