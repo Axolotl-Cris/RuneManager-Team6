b47f9b92f479636ef386e41ca0ac8f110369b550
Cristel Capetillo <cristel.capetillo@outlook.com>
2021-01-20 18:00:13 +0100

    Adds experimental Drag&Drop function
    This drag works on RuneScene. What I did: I have attached a Drag&Drop script to the Icon game object (a simple image with a blue square sprite).

---
+++ Assets/Scripts/Runes/TestDragAndDropRune.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TestDragAndDropRune : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler
+{
+
+    private RectTransform rectTransform;
+
+    private void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log("Pointer is down");
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.Log("Pointer drag started");
+    }
+    
+    public void OnDrag(PointerEventData eventData)
+    {
+        Debug.Log("Pointer is dragging");
+        rectTransform.anchoredPosition += eventData.delta;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("Pointer drag ended");
+    }
+
+}
